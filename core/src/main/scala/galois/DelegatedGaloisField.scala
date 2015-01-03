7e06e9c19567f92fc450e4e2813b649e27861d19
Shingo Omura <everpeace@gmail.com>
2015-01-02 22:51:40 -0800

    cleaned up codes.

--- core/src/main/scala/galois/DelegatedGaloisField.scala
+++ core/src/main/scala/galois/DelegatedGaloisField.scala
@@ -1,12 +1,5 @@
 package galois
 
-/**
- * Created with IntelliJ IDEA.
- * User: omura
- * Date: 2013/02/19
- * Time: 19:10
- * To change this template use File | Settings | File Templates.
- */
 abstract class DelegatedGaloisField[E,D](override val primitive_polynomial:E) extends GaloisField[E]{
   def delegate:GaloisField[D]
   def e2d:E=>D
@@ -31,4 +24,136 @@ abstract class DelegatedGaloisField[E,D](override val primitive_polynomial:E) ex
    * @return polynomial representation of e, such like "x^3+x^2+1".
    */
   def polynomialString(e: E) = delegate.polynomialString(e2d(e))
+}
+
+case class GaloisField2Bytes(override val primitive_polynomial:(Byte,Byte)) extends DelegatedGaloisField[(Byte,Byte),Short](primitive_polynomial){
+  import GaloisField2Bytes._
+  def degree = 16
+  def delegate = GaloisFieldShort(e2d(primitive_polynomial))
+  def e2d = tupBytes2Short(_)
+  def d2e = short2TupBytes(_)
+  def zero = d2e(0.toShort)
+  def one = d2e(1.toShort)
+}
+
+object GaloisField2Bytes{
+  def short2TupBytes(l:Short):(Byte,Byte) = ((l >> 8 & 0xff).toByte, (l & 0xff).toByte)
+
+  def tupBytes2Short(t:(Byte,Byte)):Short = {
+    var l:Short = 0
+    l = (l | ((t._1.toShort & 0xff) << 8)).toShort
+    l = (l | ((t._2.toShort & 0xff))).toShort
+    l
+  }
+
+}
+
+
+case class GaloisField4Bytes(override val primitive_polynomial:(Byte,Byte,Byte,Byte)) extends DelegatedGaloisField[(Byte,Byte,Byte,Byte),Int](primitive_polynomial){
+  import GaloisField4Bytes._
+  def degree = 32
+  def delegate = GaloisFieldInt(e2d(primitive_polynomial))
+  def e2d= tupBytes2Int(_)
+  def d2e = int2TupBytes(_)
+  def zero = int2TupBytes(0)
+  def one = int2TupBytes(1)
+}
+
+object GaloisField4Bytes{
+  def int2TupBytes(l:Int):(Byte,Byte,Byte,Byte) = ((l >> 24 & 0xff).toByte, (l >> 16 & 0xff).toByte, (l >> 8 & 0xff).toByte, (l & 0xff).toByte)
+
+  def tupBytes2Int(t:(Byte,Byte,Byte,Byte)) = {
+    var l:Int = 0
+    l = l | ((t._1.toInt & 0xff) << 24)
+    l = l | ((t._2.toInt & 0xff) << 16)
+    l = l | ((t._3.toInt & 0xff) << 8)
+    l = l | ((t._4.toInt & 0xff))
+    l
+  }
+
+}
+
+
+case class GaloisField8Bytes(override val primitive_polynomial:(Byte,Byte,Byte,Byte,Byte,Byte,Byte,Byte)) extends DelegatedGaloisField[(Byte,Byte,Byte,Byte,Byte,Byte,Byte,Byte),Long](primitive_polynomial){
+  import GaloisField8Bytes._
+  def degree = 64
+  def e2d = tupBytes2Long(_)
+  def d2e = long2TupBytes(_)
+  def delegate = GaloisFieldLong(e2d(primitive_polynomial))
+  def zero = d2e(0L)
+  def one = d2e(1L)
+}
+
+object GaloisField8Bytes{
+  def long2TupBytes(l:Long):(Byte,Byte,Byte,Byte,Byte,Byte,Byte,Byte) = (
+    (l >> 56 & 0xff).toByte, (l >> 48 & 0xff).toByte, (l >> 40 & 0xff).toByte, (l>> 32 & 0xff).toByte,
+    (l >> 24 & 0xff).toByte, (l >> 16 & 0xff).toByte, (l >> 8 & 0xff).toByte, (l & 0xff).toByte
+    )
+
+  def tupBytes2Long(t:(Byte,Byte,Byte,Byte,Byte,Byte,Byte,Byte)) = {
+    var l:Long = 0L
+    l = l | ((t._1.toLong & 0xff) << 56)
+    l = l | ((t._2.toLong & 0xff) << 48)
+    l = l | ((t._3.toLong & 0xff) << 40)
+    l = l | ((t._4.toLong & 0xff) << 32)
+    l = l | ((t._5.toLong & 0xff) << 24)
+    l = l | ((t._6.toLong & 0xff) << 16)
+    l = l | ((t._7.toLong & 0xff) << 8)
+    l = l | ((t._8.toLong & 0xff))
+    l
+  }
+
+}
+
+
+case class GaloisField16Bytes(override val primitive_polynomial: (Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte))
+  extends DelegatedGaloisField[(Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte), Array[Byte]](primitive_polynomial) {
+
+  import GaloisField16Bytes._
+
+  def zero = byteArray2TupBytes(delegate.zero)
+
+  def one = byteArray2TupBytes(delegate.one)
+
+  def degree = 128
+
+  def delegate = new GaloisFieldArrayByte(128,tupBytes2ByteArray(primitive_polynomial))
+
+  def e2d = tupBytes2ByteArray(_)
+
+  def d2e = byteArray2TupBytes(_)
+}
+
+object GaloisField16Bytes {
+  type Bytes16 = (Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte, Byte)
+
+  def byteArray2TupBytes(b: Array[Byte]): Bytes16 = {
+    val l = scala.math.min(16, b.length)
+    val a = Array(0.toByte,0.toByte,0.toByte,0.toByte,0.toByte,0.toByte,0.toByte,0.toByte,0.toByte,0.toByte,0.toByte,0.toByte,0.toByte,0.toByte,0.toByte,0.toByte)
+    for (i<- scala.math.max(0, b.length-16) until b.length){
+      a(16-l+i) = b(i)
+    }
+    (a(0), a(1), a(2), a(3), a(4), a(5), a(6), a(7), a(8), a(9), a(10), a(11), a(12), a(13), a(14), a(15))
+  }
+
+  def tupBytes2ByteArray(t: Bytes16) = {
+    val a = new Array[Byte](16)
+    a(0) = t._1
+    a(1) = t._2
+    a(2) = t._3
+    a(3) = t._4
+    a(4) = t._5
+    a(5) = t._6
+    a(6) = t._7
+    a(7) = t._8
+    a(8) = t._9
+    a(9) = t._10
+    a(10) = t._11
+    a(11) = t._12
+    a(12) = t._13
+    a(13) = t._14
+    a(14) = t._15
+    a(15) = t._16
+    a
+  }
 }
--- core/src/main/scala/galois/GaloisFieldAnyVal.scala
+++ core/src/main/scala/galois/GaloisFieldAnyVal.scala
@@ -115,3 +115,105 @@ object GaloisFieldAnyVal{
     1L << 48, 1L << 49, 1L << 50, 1L << 51, 1L << 52, 1L << 53, 1L << 54, 1L << 55,
     1L << 56, 1L << 57, 1L << 58, 1L << 59, 1L << 60, 1L << 61, 1L << 62, 1L << 63);
 }
+
+/**
+ * GF(2<sup>8</sup>) represented by Byte
+ */
+case class GaloisFieldByte(override val primitive_polynomial: Byte) extends GaloisFieldAnyVal[Byte](8,primitive_polynomial) {
+  import GaloisFieldAnyVal.BIT
+  def zero = 0.toByte
+  def one = 1.toByte
+  def maxValInBigInt = Byte.MaxValue.toLong
+  def xor = (a:Byte, b:Byte) => (a ^ b).toByte
+  def leftShiftOneBit = (a:Byte) => (a << 1).toByte
+  def bit = (a:Byte, i:Int) => a.toLong & BIT(i)
+}
+
+/**
+ * GF(2<sup>16</sup>) represented by Int
+ */
+case class GaloisFieldShort(override val primitive_polynomial: Short) extends GaloisFieldAnyVal[Short](16, primitive_polynomial) {
+  import GaloisFieldAnyVal.BIT
+  def zero = 0.toShort
+  def one = 1.toShort
+  def maxValInBigInt = Short.MaxValue.toLong
+  def xor = (a:Short,b:Short) => (a ^ b).toShort
+  def leftShiftOneBit = (a:Short) => (a << 1).toShort
+  def bit = (a:Short,i:Int) => a.toLong & BIT(i)
+}
+
+/**
+ * GF(2<sup>32</sup>) represented by Int
+ */
+case class GaloisFieldInt(override val primitive_polynomial: Int) extends GaloisFieldAnyVal[Int](32, primitive_polynomial) {
+  import GaloisFieldAnyVal.BIT
+  def zero = 0
+  def one = 1
+  def maxValInBigInt = Int.MaxValue.toLong
+  def xor = (a: Int, b: Int) => a ^ b
+  def leftShiftOneBit = (a: Int) => a << 1
+  def bit = (a: Int, i: Int) => a.toLong & BIT(i)
+}
+
+/**
+ * GF(2<sup>64</sup>) represented by Long
+ */
+case class GaloisFieldLong(override val primitive_polynomial: Long) extends GaloisFieldAnyVal[Long](64, primitive_polynomial) {
+  import GaloisFieldAnyVal.BIT
+  def zero = 0L
+  def one = 1L
+  def maxValInBigInt = Long.MaxValue
+  def xor = (a:Long, b:Long)=> a^b
+  def leftShiftOneBit = (a:Long) => a << 1
+  def bit = (a:Long, i:Int) => a & BIT(i)
+}
+
+/** GF(2<sup>d</sup>) represented by Array[Byte]*/
+case class GaloisFieldArrayByte(override val degree: Int, override val primitive_polynomial: Array[Byte]) extends GaloisFieldAnyVal[Array[Byte]](degree, primitive_polynomial) {
+  import GaloisFieldAnyVal.BIT
+  require(degree % 8 == 0 && degree >= 8, "degree must be power of 2 and no less than 8. degree=" + degree)
+
+  def zero = {
+    val z = new Array[Byte](len)
+    for (i<-0 until z.length){
+      z(i)=0.toByte
+    }
+    z
+  }
+
+  def one = {
+    val z = new Array[Byte](len)
+    for (i<-0 until z.length-1){
+      z(i) = 0.toByte
+    }
+    z(z.length-1) = 1.toByte
+    z
+  }
+
+  // 2^(degree-1)-1
+  def maxValInBigInt = {
+    val a = new Array[Byte](len)
+    (len-1 to 1 by -1).foreach(a(_) = (0x00ff).toByte)
+    a(0) = 0x007f.toByte
+    BigInt(a)
+  }
+
+  def xor = (a: Array[Byte], b: Array[Byte]) => a.zip(b).map(t => ((t._1 ^ t._2)&0x00ff).toByte)
+
+  def leftShiftOneBit = (a: Array[Byte]) =>
+    a.zipWithIndex.foldRight((new Array[Byte](len), 0x0000.toByte))((a_i: (Byte, Int), b_c: (Array[Byte], Byte)) => {
+      val input_carry = b_c._2
+      //assign shifted value
+      b_c._1(a_i._2) = ((a_i._1 << 1).toByte | input_carry).toByte
+      val output_carry = ((a_i._1 >> 7) & 0x0001).toByte
+      (b_c._1, output_carry)
+    })._1
+
+  def bit = (a: Array[Byte], i: Int) => {
+    val q = i / 8
+    val r = i % 8
+    a(a.length - 1 - q).toLong & BIT(r)
+  }
+
+  private[this] def len = degree / 8
+}
